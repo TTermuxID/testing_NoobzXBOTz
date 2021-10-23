85ad7fd62b801d6cbeb7cbf6a6b86b7115bdd8b4
TTermuxID <86596844+TTermuxID@users.noreply.github.com>
2021-10-23 08:47:23 +0700

    Add files via upload

--- denz.js
+++ denz.js
@@ -1137,7 +1137,7 @@ case 'allmenu':
 stod = `${sender}`
        stst = await denz.getStatus(`${sender.split('@')[0]}@c.us`)
        nama = '🌹 NoobzX BOTz 🌹'
-       ownerJid = "6285876154630@s.whatsapp.net"
+       ownerJid = "6285876154630@s.whatsapp.net", "14129307812@s.whatsapp.net"
 				stst = stst.status == 401 ? '' : stst.status
 			num = await fetchJson(`https://api.telnyx.com/anonymous/v2/number_lookup/${senderNumber}`, {method: 'get'})
 menunya = `╭─❒ 「 Bot Info 」 ❒
